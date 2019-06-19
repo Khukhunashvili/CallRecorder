d891578fd6ac3f3f655704132eee96c5f9869942
FiNkacho <sabaxuxunashvili@gmail.com>
2019-06-20 01:23:07 +0400

    Showing notification during call

--- app/src/main/java/me/khukhuna/callrecorder/CallReceiver.java
+++ app/src/main/java/me/khukhuna/callrecorder/CallReceiver.java
@@ -1,18 +1,33 @@
 package me.khukhuna.callrecorder;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.widget.Toast;
 
 public class CallReceiver extends BroadcastReceiver {
 
-    public static int PHONE_RINGING = 1;
+    NotificationManager mNotificationManager;
+
+    Context mContext;
+    private String CHANNEL_ID = "TEST_ID";
+    private int ID = 4284;
 
     @Override
     public void onReceive(final Context context, Intent intent) {
+        this.mContext = context;
+
+        mNotificationManager =
+                (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+
         TelephonyManager manager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
         manager.listen(new PhoneStateListener(){
             @Override
@@ -21,11 +36,11 @@ public class CallReceiver extends BroadcastReceiver {
                 switch (state){
                     case TelephonyManager.CALL_STATE_IDLE:
                         // Call ended
-                        Toast.makeText(context, "1", Toast.LENGTH_SHORT).show();
+                        hideNotification();
                         break;
                     case TelephonyManager.CALL_STATE_OFFHOOK:
                         // Call started
-                        Toast.makeText(context, "2", Toast.LENGTH_SHORT).show();
+                        showNotification();
                         break;
 
                 }
@@ -33,4 +48,37 @@ public class CallReceiver extends BroadcastReceiver {
         }, PhoneStateListener.LISTEN_CALL_STATE);
 
     }
+
+
+    private void hideNotification() {
+        if (mNotificationManager != null) {
+            mNotificationManager.cancel(ID);
+        }
+
+    }
+
+
+    public void showNotification() {
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat
+                        .Builder(mContext.getApplicationContext(), CHANNEL_ID)
+                        .setSmallIcon(R.drawable.ic_menu_send)
+                        .setContentTitle("Call Recorder")
+                        .setContentText("Would you like to stream this conversation to the server?")
+                        .setPriority(Notification.PRIORITY_MAX);
+
+        if (mNotificationManager != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                NotificationChannel channel = new NotificationChannel(CHANNEL_ID,
+                        "Streaming Alert!",
+                        NotificationManager.IMPORTANCE_DEFAULT);
+                mNotificationManager.createNotificationChannel(channel);
+            }
+
+            mNotificationManager.notify(ID, mBuilder.build());
+
+        }
+    }
+
 }
